94f7fcca849ec2dcd3e28261047bf7bf41a5cd55
Cameron Klepacz <cameron.klepacz@gmail.com>
2024-03-08 13:00:37 -0700

    random crap, idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+interface BandProps {
+  name: string;
+  members: string;
+  formed: number;
+}
+
+const bandNames = [
+  {
+    name: 'Beastie Boys',
+    members: 'Ad Rock, MCA, Mike D',
+    formed: 1979,
+  },
+  {
+    name: 'Big Chungus',
+    members: 'Wascally Wabbit, Elmer Fudd, Bugs Bunny',
+    formed: 1950,
+  },
+  {
+    name: 'Ur Mom',
+    members: "Name, Lettuce Rest I'm feeling beat,  Q",
+    formed: 2004,
+  },
+  {
+    name: 'Another one',
+    members: 'DJ Khalid, Potato Salad, Gosh Darn',
+    formed: 2045,
+  },
+];
+
+function Welcome() {
+  return <h1>La La La bunch of random crap</h1>;
+}
+
+class Band extends React.Component<BandProps> {
+  render() {
+    const oneBand = this.props;
+    return (
+      <div>
+        <img />
+        <h2>{oneBand.name}</h2>
+        <h3>Members: {oneBand.members}</h3>
+        <h3>Formed: {oneBand.formed}</h3>
+      </div>
+    );
+  }
+}
+
+function BandList() {
+  return (
+    <div>
+      {bandNames.map((bandNum) => (
+        <Band {...bandNum} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <BandList />
     </div>
   );
 }
